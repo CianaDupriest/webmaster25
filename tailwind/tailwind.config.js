4144a2198c9ef1d58ced8e74a3cca4a961bc95ac
CianaDupriest <145062412+CianaDupriest@users.noreply.github.com>
2024-10-24 12:39:58 -0500

    tailwind first attempt

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./pages/**/*.{html,js}',
+    './components/**/*.{html,js}'],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'themebeige': '#fef6c9',
+            'themegreen': '#93d576',
+            'themelightgrey': '#93d576',
+            'themegrey': '#878e88',
+            'themebrown': '#725752'
+        },
+    },
+    plugins: [],
+
+  }
