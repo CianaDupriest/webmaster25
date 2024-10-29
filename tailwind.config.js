12a747e1f8315b80195d2ecd505ca27eb6d871a2
CianaDupriest <145062412+CianaDupriest@users.noreply.github.com>
2024-10-29 15:06:36 -0500

    updated nav bar and stuff
    updatee dthe navbar, created a text box, and put an image there

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./pages/**/*.{html,js}',
-    './components/**/*.{html,js}'],
+    content: ["./src/**/*.{html,js}",
+
+        './*.{html,js}'],
     theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            'themebeige': '#fef6c9',
-            'themegreen': '#93d576',
-            'themelightgrey': '#93d576',
-            'themegrey': '#878e88',
-            'themebrown': '#725752'
+        extend: {
+
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'themebeige': '#fef6c9',
+                'themegreen': '#93d576',
+                'themelightgrey': '#93d576',
+                'themegrey': '#878e88',
+                'themebrown': '#725752'
+            },
         },
     },
     plugins: [],
