f75ace2cd5d1b7adb33575f50884e8737fae532e
CianaDupriest <145062412+CianaDupriest@users.noreply.github.com>
2024-12-13 13:08:59 -0600

    idk what i did ngl this was 2 months agio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js,ttf}",
-
         './*.{html,js,ttf}'],
     theme: {
         extend: {
@@ -15,11 +14,27 @@ module.exports = {
                 'themegrey': '#878e88',
                 'themebrown': '#725752'
             },
+        },
 
+        extend: {
             fontFamily: {
                 'montserrat': ['Montserrat', 'sans-serif']
             }
         },
+
+        extend: {
+            screens: {
+                'tablet': '640px',
+                // => @media (min-width: 640px) { ... }
+          
+                'laptop': '1024px',
+                // => @media (min-width: 1024px) { ... }
+          
+                'desktop': '1280px',
+                // => @media (min-width: 1280px) { ... }
+            }
+        }
+        
     },
     plugins: [],
 
